8b6f2b806ad46b524286c85a716863562cf17a3a
Jérôme Clament-Sanz <j.clamentsanz@gmail.com>
2020-10-15 15:05:01 +0200

    correction

--- app/src/main/java/com/example/testapp/fragments/AboutFragment.kt
+++ app/src/main/java/com/example/testapp/fragments/AboutFragment.kt
@@ -6,12 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.testapp.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_about.*
 
 class AboutFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(myImgView);
+
+
     }
 
 }
