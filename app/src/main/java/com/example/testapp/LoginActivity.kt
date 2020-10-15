cd6f7f5463180bef00612b7e74e479e709ff3e7e
Jérôme Clament-Sanz <j.clamentsanz@gmail.com>
2020-10-15 12:33:45 +0200

    correction

--- app/src/main/java/com/example/testapp/LoginActivity.kt
+++ app/src/main/java/com/example/testapp/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.testapp
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.testapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.testapp.fragments.AboutFragment
+import com.example.testapp.fragments.QuestionsFragment
+import com.example.testapp.fragments.SettingsFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +16,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    fun menuClick(view: View) {
+        when (view.id){
+            R.id.btnQuestions -> changeFragment(QuestionsFragment())
+            R.id.btnSettings -> changeFragment(SettingsFragment())
+            R.id.btnAbout -> changeFragment(AboutFragment())
+        }
+    }
+
+    fun changeFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit()
     }
 
 
